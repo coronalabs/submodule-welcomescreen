5cdd54dcba613761fb112b3311b8346fe85c06a4
Vlad Shcherban <vlad@coronalabs.com>
2020-11-18 19:15:33 +0200

    Changing title to Solar2D

--- extension.lua
+++ extension.lua
@@ -1,6 +1,6 @@
 extension = {
 	width = 1100,
 	height = 700,
-		title = "Welcome to Corona", -- the Simulator relies on this name
+		title = "Welcome to Solar2D", -- the Simulator relies on this name
 		showWindowTitle = false,
 	}
